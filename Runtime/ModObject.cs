a1608c75c0d9961187b3bbbe999cceecefdf8e66
outrealxrMascot <teamfort12min@gmail.com>
2021-07-08 17:57:13 +0400

    +

--- Runtime/ModObject.cs
+++ Runtime/ModObject.cs
@@ -10,6 +10,15 @@ namespace OutrealXR.HoloMod.Runtime
     [SerializeField]
     public class ModObject : MonoBehaviour
     {
+        public enum mouseCursors
+        {
+            Default,
+            Info,
+            Link,
+            Video,
+            Interactable,
+            Seat
+        }
         [HideInInspector]
         [SerializeField]
         public int type = -1;
@@ -21,10 +30,18 @@ namespace OutrealXR.HoloMod.Runtime
         public bool executeOnRecieve = false;
         public UnityEvent BeforeExecute;
         public UnityEvent AfterExecute;
+        public mouseCursors OnHoverMouseCursor;
 
+        // Start is called before the first frame update
         void Awake()
         {
             ModRegistry.Instance.RegisterModObject(this);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
         }
     }
 }
