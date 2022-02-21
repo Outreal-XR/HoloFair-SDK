45a2f0fe7dafb44134a8598446df1fce9fd76c82
thedrhax14 <48219146+thedrhax14@users.noreply.github.com>
2022-02-21 11:45:29 +0400

    Added property to animation provider

--- Runtime/Models/AnimationProvider.cs
+++ Runtime/Models/AnimationProvider.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json.Linq;
+using UnityEngine;
 
 namespace outrealxr.holomod
 {
@@ -6,6 +7,7 @@ namespace outrealxr.holomod
     {
 
         public string emoteName;
+        public Transform avatarPivotPoint;
 
         public override string ModKey => "animation";
 
