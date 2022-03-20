81b53a1fa4775bad3fd73dae7996cb413646df14
thedrhax14 <48219146+thedrhax14@users.noreply.github.com>
2022-03-20 13:29:45 +0400

    Provider is added

---
+++ Runtime/Controllers/Scoreboard/ScoreboardProvider.cs
@@ -0,0 +1,35 @@
+using Newtonsoft.Json.Linq;
+
+namespace outrealxr.holomod
+{
+    public class ScoreboardProvider : Provider
+    {
+        public Scoreboard scoreboard;
+
+        public override string ModKey => "scoreBoard";
+
+        public override string providerType => GetType().Name;
+
+        public override bool IsDirty()
+        {
+            return isDirty;
+        }
+
+        public override void SetIsDirty(bool val)
+        {
+            isDirty = val;
+        }
+
+        public override void FromJObject(JObject data)
+        {
+            scoreboard.UpdateModels(new JObject() {
+                new JProperty ("models", data.GetValue("scoreboard"))
+            });
+        }
+
+        public override JObject ToJObject()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
