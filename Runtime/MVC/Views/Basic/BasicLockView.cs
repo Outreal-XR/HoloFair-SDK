a713f602783cbadad6877dd4617a4827defc1bc4
SKhorozian <s_kh123@hotmail.com>
2022-07-26 11:22:08 +0400

    Update BasicLockView.cs

--- Runtime/MVC/Views/Basic/BasicLockView.cs
+++ Runtime/MVC/Views/Basic/BasicLockView.cs
@@ -1,11 +1,14 @@
+using TMPro;
 using UnityEngine;
 
 namespace outrealxr.holomod
 {
     public class BasicLockView : BasicStringView
     {
+        [SerializeField] private TextMeshProUGUI input;
+
         public override void Apply() {
-            
+            (model as LockModel).AttemptPassword(input.text);
         }
     }
 }
