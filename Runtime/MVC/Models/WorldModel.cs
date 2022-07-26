8eef17499142abdc4b0f92812225b70dcdca3da8
thedrhax14 <48219146+thedrhax14@users.noreply.github.com>
2022-07-26 17:38:10 +0400

    Better debugging

--- Runtime/MVC/Models/WorldModel.cs
+++ Runtime/MVC/Models/WorldModel.cs
@@ -13,18 +13,32 @@ namespace outrealxr.holomod
 
         public static WorldModel instance;
 
+#if UNITY_EDITOR
+        public TMPro.TextMeshPro debugText;
+        public Color error = Color.red, warning = Color.yellow;
+        protected Dictionary<string, TMPro.TextMeshPro> debugtexts = new Dictionary<string, TMPro.TextMeshPro>();
+#endif
+
         private void Awake()
         {
             instance = this;
         }
 
-        public void OnDataCreated(string guid, int id) {
+        /// <summary>
+        /// Updates dictionary
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <param name="id"></param>
+        /// <returns>Weather it was successfull to set an ID or not</returns>
+        public bool OnDataCreated(string guid, int id) {
             if (!GUIDsMap.ContainsKey(guid)) GUIDsMap.Add(guid, id);
             Model model = GetModel(guid);
             if (model) {
                 if (!detectedData.ContainsKey(id)) detectedData.Add(id, model);
                 detectedData[id].SetMMOItemID(id);
+                return true;
             }
+            return false;
         }
 
         public Model GetModel(string guid)
@@ -37,6 +51,23 @@ namespace outrealxr.holomod
         public abstract void WriteData(int id, string guid, JObject data);
         public abstract void ApplyData(int id, JObject data);
         public abstract JObject ReadData(string guid);
+
+        protected void ReportProblem(string guid, string msg, Vector3 pos, bool error)
+        {
+            ClearProblem(guid);
+            debugtexts.Add(guid, Instantiate(debugText, pos, Quaternion.identity, transform));
+            debugtexts[guid].text = msg;
+            debugtexts[guid].color = error ? this.error : warning;
+        }
+
+        protected void ClearProblem(string guid)
+        {
+            if (debugtexts.ContainsKey(guid))
+            {
+                if(debugtexts[guid]) Destroy(debugtexts[guid].gameObject);
+                debugtexts.Remove(guid);
+            }
+        }
 
     }
 }
