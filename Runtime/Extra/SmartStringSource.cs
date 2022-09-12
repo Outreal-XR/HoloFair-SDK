c435dfe2e13c9f986034f70171f6072f27699e53
thedrhax14 <48219146+thedrhax14@users.noreply.github.com>
2022-09-12 13:36:58 +0400

    Update SmartStringSource.cs

--- Runtime/Extra/SmartStringSource.cs
+++ Runtime/Extra/SmartStringSource.cs
@@ -6,6 +6,11 @@ namespace outrealxr.holomod.Runtime
     {
         public static SmartStringSource Instance;
 
+        private void Awake()
+        {
+            Instance = this;
+        }
+
         public abstract string GetFormattedString(string format, string guid);
     }
 }
