0b25af44fb67c31f823001cdd231bb3479a8f30e
thedrhax14 <48219146+thedrhax14@users.noreply.github.com>
2022-09-20 11:23:29 +0400

    Added RemotScoreModel

---
+++ Runtime/Extra/RemoteScoreModel.cs
@@ -0,0 +1,39 @@
+using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using Logger = Logging.Runtime.Logger;
+
+namespace outrealxr.holomod.Runtime
+{
+    public class RemoteScoreModel : WebRequestHandler
+    {
+        public void Execute(double score) => StartCoroutine(SendPostRequest(score));
+
+        private IEnumerator SendPostRequest(double score)
+        {
+            var formData = new List<IMultipartFormSection>();
+
+            formData.Add(new MultipartFormDataSection("score", score.ToString()));
+
+            var request = UnityWebRequest.Post(url, formData);
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                var jObj = JObject.Parse(request.downloadHandler.text);
+
+                foreach (var outputVar in outputVars)
+                    if (jObj.ContainsKey(outputVar.gameObject.name))
+                        outputVar.Deserialize(jObj.GetValue(outputVar.gameObject.name));
+
+                OnSuccess?.Invoke();
+            }
+            else
+            {
+                Logger.LogWarning("Failed to receive data from the server.", this);
+                OnFail?.Invoke();
+            }
+        }
+    }
+}
