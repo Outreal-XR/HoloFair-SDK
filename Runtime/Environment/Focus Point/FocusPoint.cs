e4a8caeafc65623d5c202bc18dcc67ac89f9528e
thedrhax14 <48219146+thedrhax14@users.noreply.github.com>
2022-11-22 16:57:47 +0400

    Updated FocusPoint

--- Runtime/Environment/Focus Point/FocusPoint.cs
+++ Runtime/Environment/Focus Point/FocusPoint.cs
@@ -5,6 +5,12 @@ namespace outrealxr.holomod {
 
         public bool isMain;
         public FocusPointSettings focusPointSettings;
+        [SerializeField]
+        private float _yawLimit = 45f;
+        [SerializeField]
+        private float _pitchLimit = 45;
+        public float YawLimit { get { return _yawLimit; } }
+        public float PitchLimit { get { return _pitchLimit; } }
 
     }
 }
