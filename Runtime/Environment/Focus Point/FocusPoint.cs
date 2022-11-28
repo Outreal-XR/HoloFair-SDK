f7afbb1b77631e27625f2e5392ad3292b0953418
thedrhax14 <48219146+thedrhax14@users.noreply.github.com>
2022-11-28 12:00:27 +0400

    Bug fixes

--- Runtime/Environment/Focus Point/FocusPoint.cs
+++ Runtime/Environment/Focus Point/FocusPoint.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 namespace outrealxr.holomod {
     public class FocusPoint : MonoBehaviour {
 
-        public bool isMain;
         public FocusPointSettings focusPointSettings;
         [SerializeField]
         private float _yawLimit = 45f;
@@ -11,6 +10,29 @@ namespace outrealxr.holomod {
         private float _pitchLimit = 45;
         public float YawLimit { get { return _yawLimit; } }
         public float PitchLimit { get { return _pitchLimit; } }
+
+        public static FocusPoint main;
+
+        void Awake()
+        {
+            if (IsMain()) main = this;
+        }
+
+        public void Activate()
+        {
+            Debug.LogError("[FocusPoint] Activate is not done");
+        }
+
+        public void ActivateMain()
+        {
+            if (main) main.Activate();
+            else Debug.LogError("[FocusPoint] main focus point is not assigned");
+        }
+
+        public virtual bool IsMain()
+        {
+            return false;
+        }
 
     }
 }
