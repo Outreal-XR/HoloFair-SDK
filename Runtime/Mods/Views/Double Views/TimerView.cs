d6fe45463bedf831bd9f5555e333e550ee23e3b2
SKhorozian <s_kh123@hotmail.com>
2022-12-28 16:21:38 +0400

    new timer view

---
+++ Runtime/Mods/Views/Double Views/TimerView.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.Events;
+using Logger = Logging.Runtime.Logger;
+
+namespace com.outrealxr.holomod
+{
+    public class TimerView : StringView
+    {
+        [SerializeField] private UnityEvent OnBefore;
+        [SerializeField] private UnityEvent OnDuring;
+        [SerializeField] private UnityEvent OnAfter;
+
+        [SerializeField] private double _before;
+        [SerializeField] private double _after;
+        
+        private enum State
+        {
+            Idle,
+            Before,
+            During,
+            After
+        }
+
+        [SerializeField] private State _state;
+
+        protected override void Start() {
+            base.Start();
+            UpdateTheTimeUTC();
+        }
+        
+        void Update() {
+            if (_before > 0) {
+                if (UniversalTime.Now > _after && _state != State.After) {
+                    OnAfter?.Invoke();
+                    _state = State.After;
+                } else if (UniversalTime.Now <= _before && _state != State.Before) {
+                    OnBefore?.Invoke();
+                    _state = State.Before;
+                } else if (_state != State.During) {
+                    OnDuring?.Invoke();   
+                    _state = State.During;
+                }
+            }
+        }
+
+        public override void SetValue(string value, Vector3 position) {
+            base.SetValue(value, position);
+            UpdateTheTimeUTC();
+        }
+
+        public void UpdateTheTimeUTC() {
+            var times = GetValue.Split(',');
+
+            if (times.Length != 2) {
+                Logger.LogWarning("Given string value is invalid and cannot be parsed.", this);
+                return;
+            }
+
+            _before = double.Parse(times[0]);
+            _after = double.Parse(times[1]);
+            
+            _state = State.Idle;
+        }
+    }
+}
