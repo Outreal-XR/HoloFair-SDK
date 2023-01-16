872e087eca8bf9560f415e359847f611eb950592
SKhorozian <s_kh123@hotmail.com>
2023-01-16 16:15:09 +0400

    animator view

--- Runtime/Mods/Views/String View/AnimatorView.cs
+++ Runtime/Mods/Views/String View/AnimatorView.cs
@@ -1,14 +1,45 @@
-using System;
 using System.Collections;
-using Newtonsoft.Json.Linq;
-using SaG.GuidReferences;
 using UnityEngine;
 
 namespace com.outrealxr.holomod
 {
-    //TODO fix this class
-    public class AnimatorModel : StringView
+    public class AnimatorView : StringView
     {
-        //idk what goes here
+        [Header("Local variables")]
+        public bool loop;
+        public string normalizedTimeParameterName = "progress";
+        public double startTime;
+        public int layerIndex;
+        public float elapsedTime;
+        public float animationLength;
+        public Animator animator;
+
+
+        protected override void Start() {
+            base.Start();
+            startTime = UniversalTime.Now;
+        }
+
+        public override void SetValue(string value, Vector3 position) {
+            base.SetValue(value, position);
+            startTime = UniversalTime.Now;
+            StartCoroutine(UpdateAnimationLength());
+        }
+
+        private IEnumerator UpdateAnimationLength()
+        {
+            yield return new WaitForFixedUpdate();
+            var current = animator.GetCurrentAnimatorStateInfo(layerIndex);
+            animationLength = current.length;
+            LateUpdate();
+        }
+
+        private void LateUpdate()
+        {
+            if (loop) elapsedTime = ((float)(UniversalTime.Now - startTime)) % animationLength;
+            else elapsedTime = Mathf.Clamp((float)(UniversalTime.Now - startTime), 0f, animationLength);
+            if (animationLength > 0)
+                animator.SetFloat(normalizedTimeParameterName, elapsedTime / animationLength);
+        }
     }
 }
