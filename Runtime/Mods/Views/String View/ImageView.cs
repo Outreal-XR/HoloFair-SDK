3b577423cb73f5d573156c9d0259d4ae2e1e66a9
SKhorozian <s_kh123@hotmail.com>
2023-01-19 11:13:03 +0400

    Update ImageView.cs

--- Runtime/Mods/Views/String View/ImageView.cs
+++ Runtime/Mods/Views/String View/ImageView.cs
@@ -7,8 +7,14 @@ namespace com.outrealxr.holomod
     {
         [SerializeField] protected NetworkImage _networkImage;
 
+        protected override void Start() {
+            base.Start();
+            LoadImage();
+        }
+
         public void LoadImage() {
             _networkImage.SetAndEnqueue(GetValue);
+            print("loading image");
         }
 
         public override void SetValue(string value, Vector3 position) {
