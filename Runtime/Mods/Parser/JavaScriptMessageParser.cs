5b17fdf3b0ad609854b3650fd9f1a9574a43ea28
thedrhax14 <48219146+thedrhax14@users.noreply.github.com>
2023-02-02 17:22:57 +0400

    fixed missing methods for other platforms

--- Runtime/Mods/Parser/JavaScriptMessageParser.cs
+++ Runtime/Mods/Parser/JavaScriptMessageParser.cs
@@ -4,8 +4,15 @@ namespace com.outrealxr.holomod
 {
     public abstract class JavaScriptMessageParser
     {
+#if UNITY_WEBGL
         [DllImport("__Internal")]
         protected static extern void OpenEdit(string name, string id, string edittype, string value);
+#else
+        protected static void OpenEdit(string name, string id, string edittype, string value)
+        {
+            UnityEngine.Debug.Log($"[JavaScriptMessageParser] Unable to open edit on current platform: " + UnityEngine.Application.platform);
+        } 
+#endif
         public abstract void Parse(string input);
         public abstract void OpenView();
     }
