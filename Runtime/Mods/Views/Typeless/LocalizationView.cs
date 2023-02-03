f3043e6937b1c27a55b5563d1c0013e7b9995dc3
SKhorozian <s_kh123@hotmail.com>
2023-02-03 17:48:04 +0400

    Localization view update

---
+++ Runtime/Mods/Views/Typeless/LocalizationView.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace com.outrealxr.holomod
+{
+    public class LocalizationView : View
+    {
+        public enum Locale
+        {
+            English,
+            Arabic
+        }
+
+        [SerializeField] private Locale _target;
+        [SerializeField] private UnityEvent _onLocaleValid;
+        [SerializeField] private UnityEvent _onLocaleInvalid;
+
+        public void LanguageChanged (Locale locale) {
+            if (_target == locale)
+                _onLocaleValid?.Invoke();
+            else
+                _onLocaleInvalid?.Invoke();
+        }
+    }
+}
