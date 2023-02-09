997d213c86ebb6efb7bf9ea93e6458689345089a
SKhorozian <s_kh123@hotmail.com>
2023-02-09 15:01:03 +0400

    Respawn view now respawns in a random circle

--- Runtime/Mods/Views/Typeless/RespawnView.cs
+++ Runtime/Mods/Views/Typeless/RespawnView.cs
@@ -1,9 +1,10 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace com.outrealxr.holomod
 {
-    public class RespawnView : View
+    public class RespawnView : DoubleView
     {
         [SerializeField] private Transform _respawnPoint;
 
@@ -11,7 +12,9 @@ namespace com.outrealxr.holomod
         public void RegisterAction(Action<Vector3> action) => _onRespawn = action;
 
         public void Respawn() {
-            _onRespawn?.Invoke(_respawnPoint.position);
+            var randomCircle = Random.insideUnitCircle * (float)_value;
+            var offset = new Vector3(randomCircle.x, 0, randomCircle.y);
+            _onRespawn?.Invoke(_respawnPoint.position + offset);
         }
 
         public override string Tags => "respawn";
