84e4482108754c1989baa3f1f463df3ed6703c46
SKhorozian <s_kh123@hotmail.com>
2023-02-28 10:21:26 +0400

    added to room bool

--- Runtime/Mods/Views/Typeless/Server Extention Views/ExtensionRequestView.cs
+++ Runtime/Mods/Views/Typeless/Server Extention Views/ExtensionRequestView.cs
@@ -7,17 +7,18 @@ namespace com.outrealxr.holomod
 {
     public class ExtensionRequestView : StringView
     {
+        [SerializeField] private bool _toRoom;
         [SerializeField] private SerializedObject _input;
 
-        private Action<string, SerializedObject> _onRequestSent;
+        private Action<string, SerializedObject, bool> _onRequestSent;
         public override string Tags => "extensionRequest";
 
-        public void RegisterAction(Action<string, SerializedObject> action) => _onRequestSent = action;
+        public void RegisterAction(Action<string, SerializedObject, bool> action) => _onRequestSent = action;
 
 
         public void Send()
         {
-            _onRequestSent?.Invoke(_value, _input);
+            _onRequestSent?.Invoke(_value, _input, _toRoom);
         }
     }
 }
